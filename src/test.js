5b091fc4ff5667ec0331bb57ebebde63357480d5
Eric Liu <ericliu314@gmail.com>
2018-03-30 01:00:28 -0400

    Move testing files

--- src/test.js
+++
@@ -1,9 +0,0 @@
-const dfaFile = require('./dfa.json');
-const { DFA } = require('./dfa');
-
-const dfa = new DFA(dfaFile);
-console.log(dfa.getCurrentState());
-dfa.transition('0');
-console.log(dfa.getCurrentState());
-dfa.transition('0');
-console.log(dfa.getCurrentState());
